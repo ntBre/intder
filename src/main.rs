e235e096021ec738cb85fcd329ffe6a42c0f5ebc
Brent Westbrook <brentrwestbrook@gmail.com>
2022-04-12 11:40:51 -0500

    fix infile reading without -v, update timings

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ use rust_intder::{Intder, VERBOSE};
 
 fn main() {
     let mut args = std::env::args();
-    if let Some(_) = args.find(|x| x == "-v") {
-        unsafe { VERBOSE = true };
-    }
-    let infile = match args.last() {
+    let infile = match args.next_back() {
         Some(f) => f,
         None => {
             eprintln!("expected input file name");
             std::process::exit(1);
         }
     };
+    if let Some(_) = args.find(|x| x == "-v") {
+        unsafe { VERBOSE = true };
+    }
     let intder = Intder::load(&infile);
     let new_carts = intder.convert_disps();
     let mut file07 = File::create("file07").expect("failed to create file07");
